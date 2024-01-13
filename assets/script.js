3bb7b6c75888d7b31506e486ff4e155cc8d95c25
John D <johnd@Johns-MBP.attlocal.net>
2024-01-13 16:11:22 -0600

    javascript work for randomize buttons

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+const wordList = [];
 const chosenLanguage = "es";
 const userInput = document.getElementById('inputText')
 const userOutput = document.getElementById('outputText')
@@ -32,3 +33,34 @@ translateBtn.addEventListener("click", function (event) {
   event.preventDefault();
   getTranslate(chosenLanguage, userInput.value);
 });
+
+const randomWord = async function () {
+    const url = 'https://random-word-api.p.rapidapi.com/get_word';
+       const options = {
+          method: 'GET',
+          headers: {
+              'X-RapidAPI-Key': 'a083470254msh750cc561000c23bp1b6126jsn43f4d31d7c6e',
+              'X-RapidAPI-Host': 'random-word-api.p.rapidapi.com'
+          }
+      };
+  
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        wordList.push(result.word);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const randomBtn = document.getElementById('randomBtn')
+randomBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  randomWord();
+  console.log(wordList);
+  userInput.textContent = wordList.join(' ')
+});
+
+
+
+
