6ba856786a6267f8c41a17bdc7ed46909dbe73ee
Michael Davidson <msdavid55@gmail.com>
2024-01-15 14:25:00 -0600

    changed some features in the js and html for cohesion

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,45 @@
-
-// function characterLimit() {
-//   const remainingCharacters;
-//   for (let i = maxCharacter;;) {
-//     userInput.addEventListener("keypress", function () {
-//       i--;
-//     userInput.addEventListener("keypress", "del", function(){
-//       if(i < maxCharacter) {
-//         i++;
-//       if (i === 0) {
-//         userInput.setAttribute('disabled');
+const chosenLanguage = "es";
+const userInput = document.getElementById('inputText')
+const userOutput = document.getElementById('outputText')
+const translateBtn = document.getElementById('translateBtn')
+const maxCharacter = 2500;
+const characterLimitOnScreen = document.getElementById('characterLimit');
 
 
-// const chosenLanguage = "es";
-// const userInput = document.getElementById('inputText')
-// const userOutput = document.getElementById('outputText')
-// const translateBtn = document.getElementById('translateBtn')
-// const maxCharacter = 2500;
-// const characterLimitOnScreen = document.getElementById('characterLimit');
+const getTranslate = async function (language, input) {
+  const url = 'https://google-translate113.p.rapidapi.com/api/v1/translator/text';
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': 'a083470254msh750cc561000c23bp1b6126jsn43f4d31d7c6e',
+      'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
+    },
+    body: new URLSearchParams({
+      from: 'auto',
+      to: language,
+      text: input
+    })
+  };
 
-
-// const getTranslate = async function (language, input) {
-//   const url = 'https://google-translate113.p.rapidapi.com/api/v1/translator/text';
-//   const options = {
-//     method: 'POST',
-//     headers: {
-//       'content-type': 'application/x-www-form-urlencoded',
-//       'X-RapidAPI-Key': 'a083470254msh750cc561000c23bp1b6126jsn43f4d31d7c6e',
-//       'X-RapidAPI-Host': 'google-translate113.p.rapidapi.com'
-//     },
-//     body: new URLSearchParams({
-//       from: 'auto',
-//       to: language,
-//       text: input
-//     })
-//   };
-
-//   try {
-//     const response = await fetch(url, options);
-//     const result = await response.json();
-//     userOutput.textContent = result.trans;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    userOutput.textContent = result.trans;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 translateBtn.addEventListener("click", function (event) {
   event.preventDefault();
   getTranslate(chosenLanguage, userInput.value);
 });
 
-// userInput.addEventListener("input", function(){
-//   const remainingCharacters = userInput.value.length
-//   characterLimitOnScreen.innerHTML = "Character limit: " + remainingCharacters + "/" + maxCharacter;
-// })
+userInput.addEventListener("input", function(){
+  const remainingCharacters = userInput.value.length
+  characterLimitOnScreen.innerHTML = "Character limit: " + remainingCharacters + "/" + maxCharacter;
+})
 
 
 const supportedLanguages = async function () {
