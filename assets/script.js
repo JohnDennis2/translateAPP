d5461d082b333aa2e8c8ef72689dfbcff1656a1a
John D <johnd@Johns-MBP.attlocal.net>
2024-01-16 11:23:55 -0600

    random word fix

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const wordList = [];
+let wordList = [];
 const chosenLanguage = "es";
 const userInput = document.getElementById('inputText')
 const userOutput = document.getElementById('outputText')
@@ -34,37 +34,39 @@ const getTranslate = async function (language, input) {
 
 translateBtn.addEventListener("click", function (event) {
   event.preventDefault();
+  wordList = userInput.value.split(" ")
   getTranslate(chosenLanguage, userInput.value);
 });
 
 
 const randomWord = async function () {
-    const url = 'https://random-word-api.p.rapidapi.com/get_word';
-       const options = {
-          method: 'GET',
-          headers: {
-              'X-RapidAPI-Key': 'a083470254msh750cc561000c23bp1b6126jsn43f4d31d7c6e',
-              'X-RapidAPI-Host': 'random-word-api.p.rapidapi.com'
-          }
-      };
-  
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        wordList.push(result.word);
-    } catch (error) {
-        console.error(error);
+  wordList = userInput.value.split(" ")
+  const url = 'https://random-word-api.p.rapidapi.com/get_word';
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'a083470254msh750cc561000c23bp1b6126jsn43f4d31d7c6e',
+      'X-RapidAPI-Host': 'random-word-api.p.rapidapi.com'
     }
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    wordList.push(result.word);
+    console.log(wordList);
+    userInput.value = ' ' + wordList.join(' ');
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const randomBtn = document.getElementById('randomBtn')
 randomBtn.addEventListener("click", function (event) {
   event.preventDefault();
   randomWord();
-  console.log(wordList);
-  userInput.textContent = wordList.join(' ')
 });
-
 
 userInput.addEventListener("input", function(){
   const remainingCharacters = userInput.value.length
